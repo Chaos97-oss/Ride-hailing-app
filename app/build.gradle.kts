db0bbe60f58f6d7e84ffaf5b48818aa16e169809
Paul <udegbuepaul43@gmail.com>
2025-07-27 04:53:08 +0100

    autocomplete fragment added (not yet used)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+    implementation("com.google.android.libraries.places:places:3.4.0")
+    implementation("androidx.activity:activity-compose:1.7.2")
+
     // Core libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
